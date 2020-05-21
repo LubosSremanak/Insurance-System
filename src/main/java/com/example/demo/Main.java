602541f36fb477bca7397e570b3dc41c9742f7fa
lubos <luboss@gmail.com>
2020-05-21 15:28:37 +0200

    initial commit

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -38,7 +38,6 @@ public class Main extends SpringController implements CommandLineRunner {
 
     @GetMapping("/")
     public ModelAndView drawHome() {
-
         return new ModelAndView("/html/home");
     }
 
